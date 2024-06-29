7aa5a00e874c2e51012babbcd9eb393d9ecc7517
WhiteSnake <whitesnake35711@gmail.com>
2024-06-29 14:50:21 +0530

    added recent achievements section

--- frontend/src/components/RecentAchievements.jsx
+++ frontend/src/components/RecentAchievements.jsx
@@ -1,9 +1,30 @@
 import React from 'react'
+import { events } from '../constants'
+
+const EventCard = ({event}) => {
+  return (
+    <div className='bg-gray-100 p-4 border-2 border-gray-200 rounded-lg shadow-md grid grid-cols-3 items-center'>
+      <div>
+      <h3 className='text-lg font-semibold'>{event.eventName}</h3>
+      <p className={`${event.result === 'won' ? 'text-green-700 font-bold': 'text-blue-800'}`}>{event.result}</p>
+      </div>
+      <p>{event.date}</p>
+      <p className='font-thin text-lg'>{event.location}</p>
+    </div>
+  )
+}
 
 const RecentAchievements = () => {
   return (
-    <div>
-      
+    <div className='p-4'>
+      <h1 className='text-2xl font-bold pb-4'>Recent Achievements</h1>
+    <div className='flex flex-col gap-4 '>
+      {
+        events.map((event,idx)=>(
+          <EventCard event={event} key={idx} />
+        ))
+      }
+    </div>
     </div>
   )
 }
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import Charts from "../components/Charts";
 import EventCalender from "../components/EventCalender";
+import RecentAchievements from "../components/RecentAchievements";
 
 const Home = () => {
   return (
-    <div className="flex justify-around p-10">
+    <div className="p-10">
       <div className="grid grid-cols-12 w-full h-full justify-center items-center">
         <div className="col-span-8">
           <Charts />
@@ -13,6 +14,7 @@ const Home = () => {
           <EventCalender />
         </div>
       </div>
+      <RecentAchievements />
     </div>
   );
 };
