27160dbf4a2fff9a3b53093aadc07d54ff3077cd
Your Name <you@example.com>
2024-06-30 06:14:44 +0530

    achievement models and its example

--- backend/models/achievementModel.js
+++ backend/models/achievementModel.js
@@ -9,6 +9,36 @@ const achievementSchema = new mongoose.Schema({
     type: String,
     required: true,
   },
+  location: {
+    type:String,
+    required: true,
+  },
+  link: {
+    type: String,
+    required: true,
+  },
+  date:{
+    type: Date,
+    required: true,
+  },
+  isTechnical: {
+    type: Boolean,
+    required: true,
+  },
+  result: {
+    type: String,
+    enum: ['participation', 'won'],
+    required: true,
+  },
+  proof: {
+    type: String,
+    default: null,
+  },
+  isVerified: {
+    type: Boolean,
+    default: false,
+    required: true
+  }
 });
 
 module.exports = mongoose.model("Achievement", achievementSchema);
