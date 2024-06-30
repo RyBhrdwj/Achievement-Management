ef4dc74a4b4e263343815caa76f80653dccf8f40
RyBhrdwj <25ricky08@gmail.com>
2024-06-30 17:32:31 +0530

    make controller functions

--- backend/controllers/achievementController.js
+++ backend/controllers/achievementController.js
@@ -1,6 +1,41 @@
 const achievementRepo = require("../repositories/achievementRepository");
 
-const getAchievements = async (req, res) => {
-  const achievements = await achievementRepo.getAll();
-  res.json(achievements);
-};
+class AchievementController {
+  constructor() {
+    this.achievement = new achievementRepo();
+  }
+
+  addAchievement = async (req, res) => {
+    try {
+      const achievement = await this.achievement.create(req.body);
+      res.status(201).json(achievement);
+    } catch (error) {
+      console.log("controller error : " + error);
+      res.status(400).json({ message: error.message });
+    }
+  };
+
+  deleteAchievement = async (req, res) => {
+    try {
+      const achievement = await this.achievement.destroy(req.params.id);
+      res.status(200).json(achievement);
+    } catch (error) {
+      console.log("controller error : " + error);
+      res.status(400).json({ message: error.message });
+    }
+  };a
+
+  getAchievements = async (req, res) => {
+    try {
+      const achievements = await this.achievement.getAchievementsByUserId(
+        req.params.userId
+      );
+      res.status(200).json(achievements);
+    } catch (error) {
+      console.log("controller error : " + error);
+      res.status(400).json({ message: error.message });
+    }
+  };
+}
+
+module.exports = AchievementController;
