0627b9d6eadf1aa4ade3d4f6393048aa1be6fb5e
Your Name <you@example.com>
2024-06-30 06:41:12 +0530

    usermodels added

---
+++ backend/models/userModel.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+  id: {
+    type: mongoose.Schema.Types.ObjectId,
+    default: new mongoose.Types.ObjectId(),
+    primaryKey: true,
+  },
+  displayname: {
+    type: String,
+    required: true,
+  },
+  member_role: {
+    type: String,
+    required: true,
+  },
+  team_name: {
+    type: String,
+    required: true,
+  },
+  faculty: {
+    type: Boolean,
+    required: true,
+  },
+  achievements: [{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Achievement"
+  }],
+});
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
