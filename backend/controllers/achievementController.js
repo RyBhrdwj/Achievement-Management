b201dba192eca047652394d5d6211292d4daa726
RyBhrdwj <25ricky08@gmail.com>
2024-07-03 17:18:12 +0530

    update model and controller to add update achievement functionality

--- backend/controllers/achievementController.js
+++ backend/controllers/achievementController.js
@@ -36,6 +36,50 @@ class AchievementController {
       res.status(400).json({ message: error.message });
     }
   };
+
+  getOneAchievement = async (req, res) => {
+    try {
+      const achievement = await this.achievement.getOne(req.params.id);
+
+      res.status(200).json(achievement);
+    } catch (error) {
+      console.log("controller error : " + error);
+
+      res.status(400).json({ message: error.message });
+    }
+  };
+
+  updateAchievement = async (req, res) => {
+    try {
+      const achievement = await this.achievement.updateAchievement(
+        req.params.id,
+        req.body
+      );
+
+      res.status(200).json(achievement);
+    } catch (error) {
+      console.log("controller error : " + error);
+
+      res.status(400).json({ message: error.message });
+    }
+  }
+
+  verifyAchievement = async (req, res) => {
+    try {
+      const { id, status } = req.params;
+      console.log(id, status);
+
+      const achievement = await this.achievement.updateAchievement(id, {
+        verificationStatus: status,
+      });
+
+      res.status(200).json(achievement);
+    } catch (error) {
+      console.log("controller error : " + error);
+
+      res.status(400).json({ message: error.message });
+    }
+  };
 }
 
 module.exports = new AchievementController();
