194785886778a4eae595654b063f2de770b39484
GAURJIsaheb <adityagaur086@gmail.com>
2024-07-06 11:19:55 +0530

    added navbar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,20 +1,19 @@
 import React from 'react';
-import { Outlet } from 'react-router-dom'; // Import Outlet from React Router
+import { Outlet } from 'react-router-dom'; // Import Outlet
 import Header from './components/Header';
-import SideBar from './components/SideBar'; // Import SideBar component
+import SideBar from './components/SideBar';
 import Footer from './components/Footer';
-
 
 function App() {
   return (
-     <div className='grid grid-cols-12'>
+    <div className='grid grid-cols-12'>
       <div className='sm:col-span-2'><SideBar /></div>
       <div className='col-span-12 sm:col-span-10'>
-      <Header />
-        <Outlet />
-       {/*<Footer />*/} 
-        </div>
-     </div>
+        <Header />
+        <Outlet /> {/* This will render the child routes */}
+        {/* <Footer /> */}
+      </div>
+    </div>
   );
 }
 
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,27 +1,24 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from 'react-router-dom'
-import { Home, AddDetails, Mentor } from './pages'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
+import './index.css';
+import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from 'react-router-dom';
+import { Home, AddDetails, Mentor } from './pages';
+import Notification from './pages/Notification'; // Import Notification component
 
-const Layout = () => {
-  const router = createBrowserRouter(
-    createRoutesFromElements(
-      <Route path="/" element={<App />}>
-        <Route path='' element={<Home />} />
-        <Route path='add' element={<AddDetails />} />
-        <Route path='mentor' element={<Mentor />} />
-      </Route>
-    )
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<App />}>
+      <Route path='' element={<Home />} />
+      <Route path='add' element={<AddDetails />} />
+      <Route path='mentor' element={<Mentor />} />
+      <Route path='notifications' element={<Notification />} /> {/* Add Notification route */}
+    </Route>
   )
-  return (
-    <RouterProvider router={router} />
-  )
-}
+);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Layout />
+    <RouterProvider router={router} />
   </React.StrictMode>,
-)
+);
