b1ddda970c3e9a88927a94fc0ac3adc4f038f1ef
WhiteSnake <whitesnake35711@gmail.com>
2024-07-06 16:29:40 +0530

    added responsiveness to the form

--- frontend/src/components/Form/Form1.jsx
+++ frontend/src/components/Form/Form1.jsx
@@ -1,140 +1,158 @@
-import React from 'react'
+import React from "react";
 
-const Form1 = ({details, setDetails}) => {
+const Form1 = ({ details, setDetails }) => {
   return (
-    <div className='grid gap-4'><div className="grid grid-cols-2 justify-between items-center space-x-4">
-    <fieldset className="flex flex-col space-y-2">
-      <label className="text-lg font-medium text-gray-700">
-        Event Name:
-      </label>
-      <input
-        type="text"
-        name="name"
-        value={details.name}
-        onChange={(e)=>setDetails({...details, name: e.target.value})}
-        placeholder="Enter the name of the event"
-        className="outline-none border-2 border-gray-300 px-4 py-2 rounded-md focus:border-blue-500 transition duration-150 ease-in-out"
-      />
-    </fieldset>
-    <fieldset className="flex flex-col space-y-2">
-      <label className="text-lg font-medium text-gray-700">
-        Event Date:
-      </label>
-      <input
-        type="date"
-        name="date"
-        value={details.date}
-        onChange={(e)=>setDetails({...details, date: e.target.value})}
-        placeholder="Enter the date of the event"
-        className="outline-none border-2 border-gray-300 px-4 py-2 rounded-md focus:border-blue-500 transition duration-150 ease-in-out"
-      />
-    </fieldset>
-    </div>
-    <div className="grid grid-cols-2 justify-between items-center space-x-4">
-    <fieldset className="flex flex-col space-y-2">
-      <label className="text-lg font-medium text-gray-700">
-        Type of event:
-      </label>
-      <select
-        className="py-2 px-4 bg-white border-2 border-gray-300 rounded-md"
-        value={details.type}
-        onChange={(e)=>setDetails({...details, type: e.target.value})}
-      >
-        <option value="hackathon">Hackathon</option>
-        <option value="ideathon">Ideathon</option>
-        <option value="seminar">Seminar</option>
-        <option value="meetup">Meetup</option>
-        <option value="other">Other</option>
-      </select>
-    </fieldset>
-    <fieldset className="flex flex-col space-y-2">
-      <label className={`text-lg font-medium ${details.type === 'other' ? 'text-gray-700' : 'text-gray-400'}`}>Other:</label>
-      <input
-        type="text"
-        name="name"
-        placeholder="In case of other please specify"
-        className="outline-none border-2 border-gray-300 px-4 py-2 rounded-md focus:border-blue-500 transition duration-150 ease-in-out"
-        disabled={details.type === 'other' ? false : true}
-        onChange={(e)=>setDetails({...details, otherType: e.target.value})}
-      />
-    </fieldset>
-  </div>
-  <div className='grid grid-cols-3 gap-4'>
-  <fieldset className="grid border-2 col-span-1 border-gray-300 p-4 rounded-lg">
-      <legend className="text-lg font-medium text-gray-700">Mode:</legend>
-      <div className="flex space-x-6">
-        <label className="flex items-center space-x-2 text-gray-700">
+    <div className="space-y-4">
+      <div className="grid grid-cols-1 sm:grid-cols-2 gap-4">
+        <fieldset className="flex flex-col space-y-2">
+          <label className="text-lg font-medium text-gray-700">
+            Event Name:
+          </label>
           <input
-            type="radio"
-            name="mode"
-            value="online"
-            className="form-radio text-blue-500 focus:ring-blue-500"
-            onChange={(e)=>setDetails({...details, mode: e.target.value})}
+            type="text"
+            name="name"
+            value={details.name}
+            onChange={(e) => setDetails({ ...details, name: e.target.value })}
+            placeholder="Enter the name of the event"
+            className="outline-none border-2 border-gray-300 px-4 py-2 rounded-md focus:border-blue-500 transition duration-150 ease-in-out"
           />
-          <span className="text-lg">Online</span>
-        </label>
-        <label className="flex items-center space-x-2 text-gray-700">
+        </fieldset>
+        <fieldset className="flex flex-col space-y-2">
+          <label className="text-lg font-medium text-gray-700">
+            Event Date:
+          </label>
           <input
-            type="radio"
-            name="mode"
-            value="offline"
-            className="form-radio text-blue-500 focus:ring-blue-500"
-            onChange={(e)=>setDetails({...details, mode: e.target.value})}
+            type="date"
+            name="date"
+            value={details.date}
+            onChange={(e) => setDetails({ ...details, date: e.target.value })}
+            placeholder="Enter the date of the event"
+            className="outline-none border-2 border-gray-300 px-4 py-2 rounded-md focus:border-blue-500 transition duration-150 ease-in-out"
           />
-          <span className="text-lg">Offline</span>
-        </label>
+        </fieldset>
       </div>
-      
-    </fieldset>
-    <fieldset className="grid border-2 col-span-2 border-gray-300 p-4 rounded-lg">
-      <legend className="text-lg font-medium text-gray-700">Result:</legend>
-      <div className="flex space-x-6">
-        <label className="flex items-center space-x-2 text-gray-700">
+      <div className="grid grid-cols-1 gap-4 sm:grid-cols-2">
+        <fieldset className="flex flex-col space-y-2">
+          <label className="text-lg font-medium text-gray-700">
+            Type of event:
+          </label>
+          <select
+            className="py-2 px-4 bg-white border-2 border-gray-300 rounded-md"
+            value={details.type}
+            onChange={(e) => setDetails({ ...details, type: e.target.value })}
+          >
+            <option value="hackathon">Hackathon</option>
+            <option value="ideathon">Ideathon</option>
+            <option value="seminar">Seminar</option>
+            <option value="meetup">Meetup</option>
+            <option value="other">Other</option>
+          </select>
+        </fieldset>
+        <fieldset className="flex flex-col space-y-2">
+          <label
+            className={`text-lg font-medium ${
+              details.type === "other" ? "text-gray-700" : "text-gray-400"
+            }`}
+          >
+            Other:
+          </label>
           <input
-            type="radio"
-            name="result"
-            value="winner"
-            className="form-radio text-blue-500 focus:ring-blue-500"
-            onChange={(e)=>setDetails({...details, result: e.target.value})}
+            type="text"
+            name="name"
+            placeholder="In case of other please specify"
+            className="outline-none border-2 border-gray-300 px-4 py-2 rounded-md focus:border-blue-500 transition duration-150 ease-in-out"
+            disabled={details.type === "other" ? false : true}
+            onChange={(e) =>
+              setDetails({ ...details, otherType: e.target.value })
+            }
           />
-          <span className="text-lg">Won</span>
-        </label>
-        <label className="flex items-center space-x-2 text-gray-700">
-          <input
-            type="radio"
-            name="result"
-            value="participant"
-            className="form-radio text-blue-500 focus:ring-blue-500"
-            onChange={(e)=>setDetails({...details, result: e.target.value})}
-          />
-          <span className="text-lg">Participated</span>
-        </label>
-        <label className="flex items-center space-x-2 text-gray-700">
-          <input
-            type="radio"
-            name="result"
-            value="runner up"
-            className="form-radio text-blue-500 focus:ring-blue-500"
-            onChange={(e)=>setDetails({...details, result: e.target.value})}
-          />
-          <span className="text-lg">Runner up</span>
-        </label>
+        </fieldset>
       </div>
-      
-    </fieldset>
+      <div className="grid grid-cols-1 sm:grid-cols-3 gap-4">
+        <fieldset className="grid border-2 col-span-1 border-gray-300 p-4 rounded-lg">
+          <legend className="text-lg font-medium text-gray-700">Mode:</legend>
+          <div className="flex space-x-6">
+            <label className="flex items-center space-x-2 text-gray-700">
+              <input
+                type="radio"
+                name="mode"
+                value="online"
+                className="form-radio text-blue-500 focus:ring-blue-500"
+                onChange={(e) =>
+                  setDetails({ ...details, mode: e.target.value })
+                }
+              />
+              <span className="text-lg">Online</span>
+            </label>
+            <label className="flex items-center space-x-2 text-gray-700">
+              <input
+                type="radio"
+                name="mode"
+                value="offline"
+                className="form-radio text-blue-500 focus:ring-blue-500"
+                onChange={(e) =>
+                  setDetails({ ...details, mode: e.target.value })
+                }
+              />
+              <span className="text-lg">Offline</span>
+            </label>
+          </div>
+        </fieldset>
+        <fieldset className="grid grid-cols-1 sm:col-span-2 sm:grid-cols-3 gap-4 border-2 border-gray-300 p-4 rounded-lg">
+          <legend className="text-lg font-medium text-gray-700">Result:</legend>
+          <div className="flex flex-col sm:flex-row space-y-2 sm:space-y-0 sm:space-x-6">
+            <label className="flex items-center space-x-2 text-gray-700">
+              <input
+                type="radio"
+                name="result"
+                value="winner"
+                className="form-radio text-blue-500 focus:ring-blue-500"
+                onChange={(e) =>
+                  setDetails({ ...details, result: e.target.value })
+                }
+              />
+              <span className="text-lg">Won</span>
+            </label>
+            <label className="flex items-center space-x-2 text-gray-700">
+              <input
+                type="radio"
+                name="result"
+                value="participant"
+                className="form-radio text-blue-500 focus:ring-blue-500"
+                onChange={(e) =>
+                  setDetails({ ...details, result: e.target.value })
+                }
+              />
+              <span className="text-lg">Participated</span>
+            </label>
+            <label className="flex items-center space-x-2 text-gray-700">
+              <input
+                type="radio"
+                name="result"
+                value="runner up"
+                className="form-radio text-blue-500 focus:ring-blue-500"
+                onChange={(e) =>
+                  setDetails({ ...details, result: e.target.value })
+                }
+              />
+              <span className="text-lg">Runner up</span>
+            </label>
+          </div>
+        </fieldset>
+      </div>
+      <fieldset className="flex flex-col space-y-2">
+        <label className="text-lg font-medium text-gray-700">
+          Event Venue(Link in case of online mode):
+        </label>
+        <textarea
+          name="location"
+          placeholder="Enter the location of the event"
+          className="outline-none border-2 border-gray-300 px-4 py-2 rounded-md focus:border-blue-500 transition duration-150 ease-in-out"
+          onChange={(e) => setDetails({ ...details, location: e.target.value })}
+        />
+      </fieldset>
     </div>
-  <fieldset className="flex flex-col space-y-2">
-    <label className="text-lg font-medium text-gray-700">
-      Event Venue(Link in case of online mode):
-    </label>
-    <textarea
-      name="location"
-      placeholder="Enter the location of the event"
-      className="outline-none border-2 border-gray-300 px-4 py-2 rounded-md focus:border-blue-500 transition duration-150 ease-in-out"
-      onChange={(e)=>setDetails({...details, location: e.target.value})}
-    />
-  </fieldset></div>
-  )
-}
+  );
+};
 
-export default Form1
+export default Form1;
--- frontend/src/pages/AddDetails.jsx
+++ frontend/src/pages/AddDetails.jsx
@@ -7,18 +7,22 @@ const AddDetails = () => {
   const [submit, setSubmit] = useState(false);
 
   return (
-    <div>
+    <div className="min-h-screen flex flex-col justify-center items-center">
       {submit ? (
-        <div className="flex justify-center items-center w-[83vw] h-[80vh]">
-        <div className="mx-auto my-auto max-w-2xl max-h-lg bg-gray-200 flex justify-center p-8 rounded-lg items-center gap-4 shadow">
-          <span className=""><IconContext.Provider value={{ color: "green", size: "40px" }}>
-        <BsFillSendCheckFill />
-        </IconContext.Provider></span>
-       <h1 className="text-md font-medium  flex border-l-2 border-gray-400 pl-4">Your details have been sent to the mentor for verification. Your achievement will be reflected on your dashboard once the mentor verifies it.</h1>
-        </div>
+        <div className="w-full max-w-3xl mx-auto p-8">
+          <div className="bg-gray-200 rounded-lg shadow-lg p-8 flex flex-col sm:flex-row items-center gap-4">
+            <span className="">
+              <IconContext.Provider value={{ color: "green", size: "40px" }}>
+                <BsFillSendCheckFill />
+              </IconContext.Provider>
+            </span>
+            <h1 className="text-md font-medium">
+              Your details have been sent to the mentor for verification. Your achievement will be reflected on your dashboard once the mentor verifies it.
+            </h1>
+          </div>
         </div>
       ) : (
-        <div className="p-10 m-10 bg-gray-100 rounded-lg shadow-lg max-w-3xl mx-auto">
+        <div className="p-4 sm:p-10 m-10 bg-gray-100 rounded-lg shadow-lg w-11/12 sm:max-w-3xl mx-auto">
           <h1 className="text-3xl mb-10 font-bold text-center text-gray-800">
             Enter the details of the event
           </h1>
