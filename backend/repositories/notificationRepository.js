2b733272c68c4c2ab2bc57477999b32c323efd2d
WhiteSnake <whitesnake35711@gmail.com>
2024-07-08 13:43:03 +0530

    implement notification functionality

---
+++ backend/repositories/notificationRepository.js
@@ -0,0 +1,28 @@
+const Notification = require("../models/notificationModel");
+const crudRepo = require("./crudRepo");
+
+class NotificationRepository extends crudRepo {
+  constructor() {
+    super(Notification);
+  }
+
+  getNotificationsbyUserId = async (userId) => {
+    try {
+      const notifications = await this.model.find({ user: userId });
+      return notifications;
+    } catch (err) {
+      throw new Error(err.message);
+    }
+  }
+
+  addNotification = async (data) => {
+    try {
+      const newNotification = await this.create(data);
+      return newNotification;
+    } catch (err) {
+      throw new Error(err.message);
+    }
+  }
+}
+
+module.exports = NotificationRepository;
--- backend/routes/router.js
+++ backend/routes/router.js
@@ -6,18 +6,24 @@ const {
 } = require("../middlewares/validationMiddleware");
 
 const achievementController = require("../controllers/achievementController");
+const notificationController = require("../controllers/notificationController");
 
 // Define the routes
 router.get("/", (req, res) => {
   res.send("base api route");
 });
 router.get("/achievements/:userId", achievementController.getAchievements);
+router.get("/notifications/:userId", notificationController.getNotifications);
 
 router.post(
   "/add-achievement",
   validateAchievement,
   achievementController.addAchievement
 );
+router.post(
+  "/add-notification",
+  notificationController.addNotification
+)
 router.put("/update-achievement/:id", achievementController.updateAchievement);
 
 router.patch(
