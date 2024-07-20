1f3fd2e245d7a913388608f301171ddc9c1224cc
RyBhrdwj <25ricky08@gmail.com>
2024-07-21 04:22:29 +0530

    add get all achievements

--- backend/controllers/achievementController.js
+++ backend/controllers/achievementController.js
@@ -2,25 +2,22 @@ const achievementRepo = require("../repositories/achievementRepository");
 const mentorRepo = require("../repositories/mentorRepository");
 const fs = require("fs");
 const path = require("path");
-const redis = require("redis")
+const redis = require("redis");
 const dotenv = require("dotenv");
 
-
-dotenv.config()
+dotenv.config();
 
 const redisClient = redis.createClient({
-  url: process.env.REDIS_URL // Update with your Redis server URL if different
+  url: process.env.REDIS_URL, // Update with your Redis server URL if different
 });
 redisClient.connect().catch(console.error);
-
-
 
 class AchievementController {
   constructor() {
     this.achievement = new achievementRepo();
     this.mentor = new mentorRepo();
   }
-  
+
   // addAchievement = async (req, res) => {
   //   try {
   //     const achievement = await this.achievement.create(req.body);
@@ -33,15 +30,34 @@ class AchievementController {
   // };
 
   // to handle achievement creation and file url saving in proof from s3
-  addAchievement = async(req,res)=>{
-    const{mentorId, userId, name, date, description, location, isTechnical, mode, result, verificationStatus} = req.body;
+  addAchievement = async (req, res) => {
+    const {
+      mentorId,
+      userId,
+      name,
+      date,
+      description,
+      location,
+      isTechnical,
+      mode,
+      result,
+      verificationStatus,
+    } = req.body;
     const fileURL = req.fileURL;
 
     const newAchievement = new Achievement({
       userId: userId,
-      name, date, description, location, isTechnical, mode, result, verificationStatus, proof: fileURL
+      name,
+      date,
+      description,
+      location,
+      isTechnical,
+      mode,
+      result,
+      verificationStatus,
+      proof: fileURL,
     });
-    try{
+    try {
       const savedAchievement = await newAchievement.save();
       // await redisClient.del(`savedAchievement:${userId}`;
 
@@ -50,13 +66,12 @@ class AchievementController {
       // await savedAchievement.save();
 
       res.status[200].json({
-        message: 'achivement and file uploaded',
-        achievement: savedAchievement
+        message: "achivement and file uploaded",
+        achievement: savedAchievement,
       });
-
-      } catch(err){
-        res.status(500).json({error: 'Error in uploading file'})
-      }
+    } catch (err) {
+      res.status(500).json({ error: "Error in uploading file" });
+    }
   };
 
   deleteAchievement = async (req, res) => {
@@ -94,36 +109,60 @@ class AchievementController {
     }
   };
 
+  getAllAchievements = async (req, res) => {
+    const cacheKey = "achievements:all";
+
+    try {
+      const cachedAchievements = await redisClient.get(cacheKey);
+
+      if (cachedAchievements) {
+        return res.status(200).json(JSON.parse(cachedAchievements));
+      }
+
+      const achievements = await achievementRepo.getAllAchievements();
+
+      await redisClient.set(cacheKey, JSON.stringify(achievements), {
+        EX: 3600,
+      });
+
+      res.status(200).json(achievements);
+    } catch (error) {
+      console.error("Controller error:", error);
+      res.status(500).json({ message: "Internal Server Error" });
+    }
+  };
+
   getAchievementsInCSV = async (req, res) => {
     try {
-      const achievements = await this.achievement.getAchievementsByUserIdInCSV(req.params.userId);
+      const achievements = await this.achievement.getAchievementsByUserIdInCSV(
+        req.params.userId
+      );
       // Handle the case where no achievements are found or mentor is not found
       if (!achievements || achievements.length === 0) {
-        throw new Error('Mentor not found or no achievements for the user');
+        throw new Error("Mentor not found or no achievements for the user");
       }
       // Extract headers
       const headers = Object?.keys(Object?.values(achievements[0])[2]);
       // Convert data to CSV format
-      let csv = headers.join(',') + '\n';
-      achievements.forEach(achievement => {
-        let row = headers.map(header => achievement[header] || '').join(',');
-        csv += row + '\n';
+      let csv = headers.join(",") + "\n";
+      achievements.forEach((achievement) => {
+        let row = headers.map((header) => achievement[header] || "").join(",");
+        csv += row + "\n";
       });
 
-  
       // Define the file path in the ../uploads directory
       const uploadsDir = path.join(__dirname, "../uploads");
-  
+
       // Create the uploads directory if it doesn't exist
       if (!fs.existsSync(uploadsDir)) {
         fs.mkdirSync(uploadsDir, { recursive: true });
       }
-  
+
       const filePath = path.join(uploadsDir, "achievements.csv");
       const fileUrl = `http://localhost:3000/uploads/achievements.csv`;
       // Write the CSV to a file
       fs.writeFileSync(filePath, csv);
-  
+
       res.status(200).json({ achievements, fileUrl });
     } catch (error) {
       console.error("Controller error:", error);
@@ -160,66 +199,73 @@ class AchievementController {
       const { mentorId, status } = req.params;
       const mentor = await this.mentor.getMentorById(mentorId);
       const validStatuses = ["pending", "accepted", "rejected"];
-  
+
       if (!validStatuses.includes(status)) {
         throw new Error("Invalid status");
       }
-  
-      const achievements = await this.achievement.getAchievementsByUserIdsAndStatus(
-        mentor.studentUserIds,
-        status
-      );
-  
+
+      const achievements =
+        await this.achievement.getAchievementsByUserIdsAndStatus(
+          mentor.studentUserIds,
+          status
+        );
+
       // Handle the case where no achievements are found
       if (!achievements || achievements.length === 0) {
-        throw new Error('No achievements found for the given status');
+        throw new Error("No achievements found for the given status");
       }
-  
+
       // Extract headers for user and achievement info
       const userHeaders = Object.keys(Object.values(achievements[0].userId)[2]);
-      
-      const achievementHeaders = Object.keys(Object.values(achievements[0])[2]).filter(key => key !== 'userId');
-      console.log(achievementHeaders)
+
+      const achievementHeaders = Object.keys(
+        Object.values(achievements[0])[2]
+      ).filter((key) => key !== "userId");
+      console.log(achievementHeaders);
       // Create CSV data
-      let csv = 'Student Information\n';
-      csv += userHeaders.join(',') + '\n';
+      let csv = "Student Information\n";
+      csv += userHeaders.join(",") + "\n";
       let uniqueStudents = new Set();
-      achievements.forEach(achievement => {
-        let userRow = userHeaders.map(header => achievement.userId[header] || '').join(',');
+      achievements.forEach((achievement) => {
+        let userRow = userHeaders
+          .map((header) => achievement.userId[header] || "")
+          .join(",");
         if (!uniqueStudents.has(userRow)) {
-          csv += userRow + '\n';
+          csv += userRow + "\n";
           uniqueStudents.add(userRow);
         }
       });
-  
-      csv += '\nAchievements\n';
-      csv += achievementHeaders.join(',') + '\n';
-      achievements.forEach(achievement => {
-        let achievementRow = achievementHeaders.map(header => {
-          if (header === 'userId') {
-            return achievement[header] ? achievement[header]._id : '';
-          }
-          return achievement[header] || '';
-        }).join(',');
-        csv += achievementRow + '\n';
+
+      csv += "\nAchievements\n";
+      csv += achievementHeaders.join(",") + "\n";
+      achievements.forEach((achievement) => {
+        let achievementRow = achievementHeaders
+          .map((header) => {
+            if (header === "userId") {
+              return achievement[header] ? achievement[header]._id : "";
+            }
+            return achievement[header] || "";
+          })
+          .join(",");
+        csv += achievementRow + "\n";
       });
-  
+
       // Log the generated CSV
       console.log(csv);
-  
+
       // Define the file path in the ../uploads directory
       const uploadsDir = path.join(__dirname, "../uploads");
-  
+
       // Create the uploads directory if it doesn't exist
       if (!fs.existsSync(uploadsDir)) {
         fs.mkdirSync(uploadsDir, { recursive: true });
       }
-  
+
       const filePath = path.join(uploadsDir, "student_achievements.csv");
       const fileUrl = `http://localhost:3000/uploads/student_achievements.csv`;
       // Write the CSV to a file
       fs.writeFileSync(filePath, csv);
-  
+
       res.status(200).json({ achievements, fileUrl });
     } catch (error) {
       console.error("Controller error:", error);
